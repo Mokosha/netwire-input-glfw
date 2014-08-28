5538dc0e207709b640c55155959cf99441598720
Pavel Krajcevski <Krajcevski@gmail.com>
2014-08-28 01:57:29 -0400

    Renaming

--- lib/FRP/Netwire/Input/GLFW.hs
+++ lib/FRP/Netwire/Input/GLFW.hs
@@ -36,8 +36,8 @@ instance Key GLFW.Key
 instance MouseButton GLFW.MouseButton
 
 -- !FIXME! Perhaps this is better in its own newtype
-type GLFWInput a = State GLFWInputState a
-type GLFWInputT m a = StateT GLFWInputState m a
+type GLFWInput = State GLFWInputState
+type GLFWInputT m = StateT GLFWInputState m
 
 instance (Functor m, Monad m) =>
          MonadInput GLFW.Key GLFW.MouseButton (StateT GLFWInputState m) where
@@ -94,7 +94,7 @@ withPressedButton input mb fn = if isButtonPressed mb input then fn else id
 debounceButton :: GLFW.MouseButton -> GLFWInputState -> GLFWInputState
 debounceButton mb = (\input -> input { mbPressed = Set.delete mb (mbPressed input) })
 
-data InputGLFWControl = IptCtl (TVar GLFWInputState) GLFW.Window
+data GLFWInputControl = IptCtl (TVar GLFWInputState) GLFW.Window
 
 -- Returns a snapshot of the input
 setCursorToWindowCenter :: GLFW.Window -> IO ()
@@ -102,10 +102,10 @@ setCursorToWindowCenter win = do
   (w, h) <- GLFW.getWindowSize win
   GLFW.setCursorPos win (fromIntegral w / 2.0) (fromIntegral h / 2.0)
 
-getInput :: InputGLFWControl -> IO(GLFWInputState)
+getInput :: GLFWInputControl -> IO(GLFWInputState)
 getInput (IptCtl var _) = readTVarIO var
 
-setInput :: InputGLFWControl -> GLFWInputState -> IO ()
+setInput :: GLFWInputControl -> GLFWInputState -> IO ()
 setInput (IptCtl var win) ipt = do
   case (cursorPos ipt) of
     Just _ -> return ()
@@ -117,13 +117,13 @@ resetCursorPos = (\input -> input { cursorPos = Nothing })
 
 --------------------------
 
-scrollCallback :: InputGLFWControl -> GLFW.Window -> Double -> Double -> IO ()
+scrollCallback :: GLFWInputControl -> GLFW.Window -> Double -> Double -> IO ()
 scrollCallback (IptCtl ctl _) _ xoff yoff = atomically $ modifyTVar' ctl updateScroll
   where
     updateScroll :: GLFWInputState -> GLFWInputState
     updateScroll = (\input -> input { scrollAmt = (xoff, yoff) })
 
-keyCallback :: InputGLFWControl -> GLFW.Window ->
+keyCallback :: GLFWInputControl -> GLFW.Window ->
                GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys -> IO ()
 keyCallback (IptCtl ctl _) _ key _ keystate _ = atomically $ modifyTVar' ctl modifyKeys
   where
@@ -136,7 +136,7 @@ keyCallback (IptCtl ctl _) _ key _ keystate _ = atomically $ modifyTVar' ctl mod
       GLFW.KeyState'Released -> updateKeys $ Set.delete key
       _ -> id
 
-mouseButtonCallback :: InputGLFWControl -> GLFW.Window ->
+mouseButtonCallback :: GLFWInputControl -> GLFW.Window ->
                        GLFW.MouseButton -> GLFW.MouseButtonState ->
                        GLFW.ModifierKeys -> IO ()
 mouseButtonCallback (IptCtl ctl _) _ button state _ =
@@ -155,14 +155,14 @@ mouseButtonCallback (IptCtl ctl _) _ button state _ =
 -- regardless of application ... we should really expose this to the user
 -- somehow...
 
-cursorPosCallback :: InputGLFWControl -> GLFW.Window -> Double -> Double -> IO ()
+cursorPosCallback :: GLFWInputControl -> GLFW.Window -> Double -> Double -> IO ()
 cursorPosCallback (IptCtl ctl _) win x y = do
   (w, h) <- GLFW.getWindowSize win
   let xf = newRangeC (double2Float x) (0, fromIntegral w) (-1, 1)
       yf = newRangeC (double2Float y) (0, fromIntegral h) (-1, 1)
   atomically $ modifyTVar' ctl (\ipt -> ipt { cursorPos = Just (xf, yf)})
 
-mkInputControl :: GLFW.Window -> IO (InputGLFWControl)
+mkInputControl :: GLFW.Window -> IO (GLFWInputControl)
 mkInputControl win = do
 
   GLFW.setCursorInputMode win GLFW.CursorInputMode'Disabled
@@ -175,8 +175,9 @@ mkInputControl win = do
   GLFW.setMouseButtonCallback win (Just $ mouseButtonCallback ctl)
   return ctl
 
-pollGLFW :: GLFWInputState -> InputGLFWControl -> IO (GLFWInputState)
+pollGLFW :: GLFWInputState -> GLFWInputControl -> IO ()
 pollGLFW ipt iptctl = do
-  setInput iptctl ipt
+  if (cursorMode ipt) == CursorMode'Disabled
+    then setInput iptctl (resetCursorPos ipt)
+    else setInput iptctl ipt
   GLFW.pollEvents
-  getInput iptctl
